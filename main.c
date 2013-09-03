19938ea66e11501299719b32686c8a70d7365896
HernanEche <hernan.eche@gmail.com>
2013-09-03 14:24:29 -0300

    Added comment

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 #define favor ){ int i=0; do{
 #define escriba h: i++; printf(
 #define unas );}while(i<
-#define veces );
 #define diez 10
+#define veces );
 #define en ;
 #define la ;
 #define pantalla }
 
-Computadora por favor escriba " Hola " unas diez veces en la pantalla
+Computadora por favor escriba " Hola " unas diez veces en la pantalla // Lenguaje C puro.
