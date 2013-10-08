253b9aaf116988eae68f3b5ed23e0d9048e172e4
Hernán Eche <hernan_eche@yahoo.com.ar>
2013-10-08 00:50:59 -0300

    Update main.c
    deleted unused tag, change comment to iso C89/C90

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #define Computadora int
 #define por main(
 #define favor ){ int i=0; do{
-#define escriba h: i++; printf(
+#define escriba i++; printf(
 #define unas );}while(i<
 #define diez 10
 #define veces );
@@ -10,4 +10,4 @@
 #define la ;
 #define pantalla }
 
-Computadora por favor escriba " Hola " unas diez veces en la pantalla // Lenguaje C puro.
+Computadora por favor escriba " Hola " unas diez veces en la pantalla /* Lenguaje C puro. */
